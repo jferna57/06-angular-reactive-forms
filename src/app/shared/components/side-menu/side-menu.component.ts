d721132a4a882aa3bb63dccf96cf4c1f4c25d695
Juan Carlos Fernández <juan.carlos.fernandez@ricoh.es>
2025-05-16 16:26:15 +0200

    feat: enhance side menu with dynamic routing and structure layout in app component

--- src/app/shared/components/side-menu/side-menu.component.ts
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -1,8 +1,37 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+import { authRoutes } from '../../../auth/auth.routes';
+import { reactiveRoutes } from '../../../reactive/reactive.routes';
+
+interface MenuItem {
+  title: string;
+  route: string;
+
+}
+const reactiveItems = reactiveRoutes[0].children ?? [];
+const authItems = authRoutes[0].children ?? [];
 
 @Component({
   selector: 'app-side-menu',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu.component.html',
 })
-export class SideMenuComponent { }
+export class SideMenuComponent {
+
+  reactiveMenu: MenuItem[] = reactiveItems.filter(item => item.path != '**').map((item) => ({
+    title: `${item.title}`,
+    route: `reactive/${item.path}`,
+  }));
+
+  authMenu: MenuItem[] = [{
+    title: 'Autenticación',
+    route: 'auth'
+  }];
+
+  countryMenu: MenuItem[] = [{
+    title: 'Country',
+    route: 'country'
+  }];
+
+}
